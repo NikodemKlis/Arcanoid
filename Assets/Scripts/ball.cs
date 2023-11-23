ba1fdb49bd203f5aa928a6f4da5f67bec92c5c57
NikodemKlis <nikodemklll342@gmail.com>
2023-11-23 11:16:53 +0100

    Arcanoid v0.1.9 - Balance Changes
    - Bug fix: Ball speed stacking infinitely
    - Bug fix: Being unable to win (now set to 5 score)
    - Added: Start game key set to A
    - Added: Score counter

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -1,13 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class ball : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI tmp;
+    [SerializeField] Int16 i = 0;
+    [SerializeField] bool startGame = false;
     private void OnCollisionEnter2D(Collision2D obj) { 
 
         if (obj.gameObject.CompareTag("Block")){
+            i++;
             Destroy(obj.gameObject);
+            tmp.text = "Score: "+i;
         }
+        if(i == 5) {
+            SceneManager.LoadScene(1);
+        }
+    }
+    private void Update()
+    {
+        if (startGame == false) {
+            if (Input.GetKeyDown(KeyCode.A)){
+                startGame = true;
+                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 15, 0);
+            }
+        }
+
     }
 }
